f0010764dd133389169156e06c1c643759591213
Lai Tin Lok <adrianlai2000@privatemail.com>
2021-11-07 21:03:01 +0800

    Add existing file

--- Game Client.py
+++ Game Client.py
@@ -8,5 +8,5 @@ username= input("Please input your user name:")
 password=input("Please input your password:")
 msg="/login "+username+" "+password
 clientSocket.send(msg.encode('ascii'))
-rmsg = clientSocket.recv(1024).decode
+rmsg = clientSocket.recv(1024).decode()
 print(rmsg)
