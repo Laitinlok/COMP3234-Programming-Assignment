3798217b664bd9085c3438bc2a85ec888447385e
Laitinlok <63400461+Laitinlok@users.noreply.github.com>
2021-11-07 20:36:04 +0800

    Add files via upload

---
+++ Game Client.py
@@ -0,0 +1,11 @@
+import socket
+
+clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serverName = "127.0.0.1"
+serverPort = 12000
+clientSocket.connect( (serverName, serverPort) )
+username= input("Please input your user name:")
+password=input("Please input your password:")
+msg="/login "+username+" "+password
+clientSocket.send(msg.encode('ascii'))
+rmsg = clientSocket.recv(1024)
---
+++ Game Server.py
@@ -0,0 +1,36 @@
+import socket
+
+import threading
+
+class ServerThread(threading.Thread):
+	def __init__(self, client):
+		threading.Thread.__init__(self)
+		self.client = client
+
+	def run(self):
+		connectionSocket, addr = self.client    
+		rmsg = connectionSocket.recv(1024).decode()
+		recv=rmsg.split()
+		print(recv[0])
+		if recv[0]=="/login":
+			username=recv[1]
+			password=recv[2]
+			print(username+":"+password)
+		
+		
+class ServerMain:
+	def server_run(self):  
+		serverPort = 12000
+		serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		serverSocket.bind( ("", serverPort) )
+		serverSocket.listen(5)
+		print("The server is ready to receive")
+		while True:
+			client = serverSocket.accept()
+			t = ServerThread(client)
+			t.start()
+
+
+if __name__ == '__main__':
+	server = ServerMain()
+	server.server_run()
